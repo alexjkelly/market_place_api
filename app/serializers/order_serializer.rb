207a5e576387ce07868a6b7be715c420054c4b93
Alex Kelly <alex.kelly@william-lee.com>
2015-05-08 16:47:33 +0100

    Adds caching for the serializers

--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -1,4 +1,9 @@
 class OrderSerializer < ActiveModel::Serializer
+  cached
   attributes :id, :total
   has_many :products, serializer: OrderProductSerializer
+  
+  def cache_key
+  	[object, scope]
+  end
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,11 @@
 class UserSerializer < ActiveModel::Serializer
+  cached
+  
   attributes :id, :email, :created_at, :updated_at, :auth_token
 
   has_many :products, embed: :ids
+  
+  def cache_key
+  	[object, scope]
+  end
 end
