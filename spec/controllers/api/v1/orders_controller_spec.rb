8d8f0d2e4fa3cbcb131669722bcdbbd72d206ddd
Alex Kelly <alex.kelly@william-lee.com>
2015-05-08 14:19:51 +0100

    Adds the show action for order

--- spec/controllers/api/v1/orders_controller_spec.rb
+++ spec/controllers/api/v1/orders_controller_spec.rb
@@ -15,4 +15,19 @@ describe Api::V1::OrdersController do
 
 		it { should respond_with 200 }
 	end
+	
+	describe "GET #show" do
+		before(:each) do
+			current_user = FactoryGirl.create :user
+			api_authorization_header current_user.auth_token
+			@order = FactoryGirl.create :order, user: current_user
+			get :show, user_id: current_user, id: @order
+		end
+		
+		it "returns the user order record matching the id" do
+			expect(json_response[:order][:id]).to eql @order.id
+		end
+		
+		it { should respond_with 200 }
+	end
 end
